11eb7127551b6bdaa46b820b94c8b80362cf2857
mayronh <mayron.hen@gmail.com>
2020-02-26 16:48:11 -0200

    Colocando o formulário de comentários na mesma página do post, editando labels dos formulários e mensagem de sucesso

--- blog/form.py
+++ blog/form.py
@@ -6,6 +6,12 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'post_image', 'text', )
+        # Alterando os labels do formulário
+        labels = {
+        'title': "Título:",
+        'post_image': "Imagem de destaque:",
+        'text': "Texto:",
+        }
 
 
 class CommentForm(forms.ModelForm):
@@ -13,3 +19,7 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('author', 'text', )
+        labels = {
+        'author': "Nome:",
+        'text': "Comentário:",
+        }
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from .models import Post, Comment
 from .form import PostForm, CommentForm
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.db.models import Q
 
 # Create your views here.
@@ -15,7 +16,18 @@ def post_list(request):
 
 def post_detail(request,pk):
     post = get_object_or_404(Post, pk = pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    # Colocando o formulario de comentário na mesma página.
+    if request.method == "POST" :
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            messages.success(request, 'Comentário enviado para aprovação com sucesso')
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/post_detail.html', {'post': post, 'form': form})
 
 @login_required
 def post_new(request):
